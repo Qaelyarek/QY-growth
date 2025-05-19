18497afcc8291ca1dfa57dde81d3a4a294222c50
Mr. Yarek <admin@qygrowth.com>
2025-05-19 00:20:30 -0400

    Deleted index.css

--- src/lib/env.ts
+++
@@ -1,8 +0,0 @@
-export const env = {
-  VAPI_PUBLIC_KEY: import.meta.env.VITE_VAPI_PUBLIC_KEY as string,
-  VAPI_ASSISTANT_ID: import.meta.env.VITE_VAPI_ASSISTANT_ID as string
-};
-
-if (!env.VAPI_PUBLIC_KEY || !env.VAPI_ASSISTANT_ID) {
-  throw new Error('Missing required VAPI environment variables');
-}
